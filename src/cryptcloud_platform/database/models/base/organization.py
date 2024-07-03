e92a339c23307aa47e0c6d4cfebd354bb63d1738
cam <cammclain@skiff.com>
2024-07-02 23:55:28 -0500

    meta bullshit

--- src/cryptcloud_platform/database/models/base/organization.py
+++ src/cryptcloud_platform/database/models/base/organization.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from datetime import date, datetime
+from datetime import datetime
 from typing import TYPE_CHECKING
 
 from advanced_alchemy.base import UUIDAuditBase

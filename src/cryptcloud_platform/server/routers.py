ceda52e1c72739734b32f922c42c2381851662a2
cam <cammclain@skiff.com>
2024-07-02 21:33:14 -0500

    set up controllers handlers

---
+++ src/cryptcloud_platform/server/routers.py
@@ -0,0 +1,17 @@
+"""Application Modules"""
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+
+## Import Controllers ##
+
+# from app.fgjgksgjdfk whatever import
+
+if TYPE_CHECKING:
+    from litestar.types import ControllerRouterHandler
+    
+route_handlers: list[ControllerRouterHandler] = [
+    
+    # Controllers
+]

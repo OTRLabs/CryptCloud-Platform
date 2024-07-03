780afe2e6c4864a66709d210ecca1beff816a45e
cam <cammclain@skiff.com>
2024-07-03 05:34:16 -0500

    DO NOT LOOK AT THIS CHANGE 😢

---
+++ src/cryptcloud_platform/domain/accounts/guards.py
@@ -0,0 +1,102 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Any
+
+from litestar.exceptions import PermissionDeniedException
+from litestar.security.jwt import Token
+
+from ...config import constants
+from ...config.app import alchemy
+from ...config.base import get_settings
+from ...database.models.base.user import User
+from . import urls
+from .dependencies import provide_users_service
+
+if TYPE_CHECKING:
+    from litestar.connection import ASGIConnection
+    from litestar.handlers.base import BaseRouteHandler
+    from litestar.security.jwt import Token
+
+
+__all__ = ("requires_superuser", "requires_active_user", "requires_verified_user", "current_user_from_token", "auth")
+
+
+settings = get_settings()
+
+
+def requires_active_user(connection: ASGIConnection, _: BaseRouteHandler) -> None:
+    """Request requires active user.
+
+    Verifies the request user is active.
+
+    Args:
+        connection (ASGIConnection): HTTP Request
+        _ (BaseRouteHandler): Route handler
+
+    Raises:
+        PermissionDeniedException: Permission denied exception
+    """
+    if connection.user.is_active:
+        return
+    msg = "Inactive account"
+    raise PermissionDeniedException(msg)
+
+
+def requires_superuser(connection: ASGIConnection, _: BaseRouteHandler) -> None:
+    """Request requires active superuser.
+
+    Args:
+        connection (ASGIConnection): HTTP Request
+        _ (BaseRouteHandler): Route handler
+
+    Raises:
+        PermissionDeniedException: Permission denied exception
+
+    Returns:
+        None: Returns None when successful
+    """
+    if connection.user.is_superuser:
+        return
+    raise PermissionDeniedException(detail="Insufficient privileges")
+
+
+def requires_verified_user(connection: ASGIConnection, _: BaseRouteHandler) -> None:
+    """Verify the connection user is a superuser.
+
+    Args:
+        connection (ASGIConnection): Request/Connection object.
+        _ (BaseRouteHandler): Route handler.
+
+    Raises:
+        PermissionDeniedException: Not authorized
+
+    Returns:
+        None: Returns None when successful
+    """
+    if connection.user.is_verified:
+        return
+    raise PermissionDeniedException(detail="User account is not verified.")
+
+
+async def current_user_from_token(token: Token, connection: ASGIConnection[Any, Any, Any, Any]) -> User | None:
+    """Lookup current user from local JWT token.
+
+    Fetches the user information from the database
+
+
+    Args:
+        token (str): JWT Token Object
+        connection (ASGIConnection[Any, Any, Any, Any]): ASGI connection.
+
+
+    Returns:
+        User: User record mapped to the JWT identifier
+    """
+    service = await anext(provide_users_service(alchemy.provide_session(connection.app.state, connection.scope)))
+    user = await service.get_one_or_none(email=token.sub)
+    return user if user and user.is_active else None
+
+
+
+
+auth = "authauthauth come fix this fr what the fuck"

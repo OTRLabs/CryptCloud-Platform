484c7eea00ee4906f339d3e0d402b3e535e94cad
cam <cammclain@skiff.com>
2024-07-02 22:38:48 -0500

    refactor app.py

--- src/cryptcloud_platform/config/app.py
+++ src/cryptcloud_platform/config/app.py
@@ -21,3 +21,107 @@ from litestar_vite import ViteConfig
 from .base import get_settings
 
 settings = get_settings()
+
+alchemy = SQLAlchemyAsyncConfig(
+    engine_instance=settings.db.get_engine(),
+    before_send_handler=async_autocommit_before_send_handler,
+    session_config=AsyncSessionConfig(expire_on_commit=False),
+    alembic_config=AlembicAsyncConfig(
+        version_table_name=settings.db.MIGRATION_DDL_VERSION_TABLE,
+        script_config=settings.db.MIGRATION_CONFIG,
+        script_location=settings.db.MIGRATION_PATH,
+    ),
+)
+'''vite = ViteConfig(
+    bundle_dir=settings.vite.BUNDLE_DIR,
+    resource_dir=settings.vite.RESOURCE_DIR,
+    template_dir=settings.vite.TEMPLATE_DIR,
+    use_server_lifespan=settings.vite.USE_SERVER_LIFESPAN,
+    dev_mode=settings.vite.DEV_MODE,
+    hot_reload=settings.vite.HOT_RELOAD,
+    is_react=settings.vite.ENABLE_REACT_HELPERS,
+    port=settings.vite.PORT,
+    host=settings.vite.HOST,
+)
+saq = SAQConfig(
+    redis=settings.redis.client,
+    web_enabled=settings.saq.WEB_ENABLED,
+    worker_processes=settings.saq.PROCESSES,
+    use_server_lifespan=settings.saq.USE_SERVER_LIFESPAN,
+    queue_configs=[
+        QueueConfig(
+            name="system-tasks",
+            tasks=["app.domain.system.tasks.system_task", "app.domain.system.tasks.system_upkeep"],
+            scheduled_tasks=[
+                CronJob(
+                    function="app.domain.system.tasks.system_upkeep",
+                    unique=True,
+                    cron="0 * * * *",
+                    timeout=500,
+                ),
+            ],
+        ),
+        QueueConfig(
+            name="background-tasks",
+            tasks=["app.domain.system.tasks.background_worker_task"],
+            scheduled_tasks=[
+                CronJob(
+                    function="app.domain.system.tasks.background_worker_task",
+                    unique=True,
+                    cron="* * * * *",
+                    timeout=300,
+                ),
+            ],
+        ),
+    ],
+)
+
+log = StructlogConfig(
+    structlog_logging_config=StructLoggingConfig(
+        log_exceptions="always",
+        standard_lib_logging_config=LoggingConfig(
+            root={"level": logging.getLevelName(settings.log.LEVEL), "handlers": ["queue_listener"]},
+            loggers={
+                "uvicorn.access": {
+                    "propagate": False,
+                    "level": settings.log.UVICORN_ACCESS_LEVEL,
+                    "handlers": ["queue_listener"],
+                },
+                "uvicorn.error": {
+                    "propagate": False,
+                    "level": settings.log.UVICORN_ERROR_LEVEL,
+                    "handlers": ["queue_listener"],
+                },
+                "granian.access": {
+                    "propagate": False,
+                    "level": settings.log.GRANIAN_ACCESS_LEVEL,
+                    "handlers": ["queue_listener"],
+                },
+                "granian.error": {
+                    "propagate": False,
+                    "level": settings.log.GRANIAN_ERROR_LEVEL,
+                    "handlers": ["queue_listener"],
+                },
+                "saq": {
+                    "propagate": False,
+                    "level": settings.log.SAQ_LEVEL,
+                    "handlers": ["queue_listener"],
+                },
+                "sqlalchemy.engine": {
+                    "propagate": False,
+                    "level": settings.log.SQLALCHEMY_LEVEL,
+                    "handlers": ["queue_listener"],
+                },
+                "sqlalchemy.pool": {
+                    "propagate": False,
+                    "level": settings.log.SQLALCHEMY_LEVEL,
+                    "handlers": ["queue_listener"],
+                },
+            },
+        ),
+    ),
+    middleware_logging_config=LoggingMiddlewareConfig(
+        request_log_fields=["method", "path", "path_params", "query"],
+        response_log_fields=["status_code"],
+    ),
+)'''

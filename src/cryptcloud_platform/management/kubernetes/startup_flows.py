5f1b91b9a55685ad600a4219d134a31aeeaaeb4c
cam <cammclain@skiff.com>
2024-07-03 08:03:25 -0500

    adding `launch_kubernetes_cluster`

--- src/cryptcloud_platform/management/kubernetes/startup_flows.py
+++ src/cryptcloud_platform/management/kubernetes/startup_flows.py
@@ -1,4 +1,11 @@
 from __future__ import annotations
 
-
+from pick import pick
 from kubernetes import client, config, watch
+from datetime import datetime
+from kubernetes.client import configuration
+
+
+def launch_kubernetes_cluster() -> None:
+    
+    print(f"Launching Kubernetes cluster @ {datetime.now()}")
